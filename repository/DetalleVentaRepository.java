3ce5a06cbde1b53beef7f48effa1d6906bb362bb
cmanuel-2022447 <cmanuel-2022447@kinal.edu.gt>
2026-04-14 18:57:05 -0600

    fix: eliminar configuración incorrecta en directorio repository

--- repository/DetalleVentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cristianmanuel.Kinalapp.repository;
-
-import com.cristianmanuel.Kinalapp.entity.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-}
